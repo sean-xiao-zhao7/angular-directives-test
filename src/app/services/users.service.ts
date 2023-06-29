3fefcf6c27247c3747c43f9ca1e8f484ccfd8111
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-06-29 17:43:51 -0400

    Add dummy user

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UsersService {
+  users = [
+    {
+      name: 'Test',
+      email: 'test@test.com',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 }
