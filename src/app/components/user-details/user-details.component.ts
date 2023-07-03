ee89f897f64be7297d29ebc2f409f9805e4b1f3b
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-07-03 19:26:59 -0400

    Use any for paramsSub type

--- src/app/components/user-details/user-details.component.ts
+++ src/app/components/user-details/user-details.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user-details',
@@ -9,7 +8,7 @@ import { Subscription } from 'rxjs';
 })
 export class UserDetailsComponent implements OnInit, OnDestroy {
   userId: string = '';
-  private paramsSub: Subscription;
+  private paramsSub: any;
 
   constructor(private route: ActivatedRoute) {}
 
