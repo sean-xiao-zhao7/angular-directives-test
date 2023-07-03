8263e1a87aefd4ac98d19bf9d6a66b5bea0a01b4
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-07-03 18:38:33 -0400

    Add appRoutes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { HighlightDirective } from './directives/highlight.directive';
 import { UnlessDirective } from './directives/unless.directive';
 import { UsersService } from './services/users.service';
 import { LoggingService } from './services/logging.service';
+import { Routes } from '@angular/router';
+import { appRoutes } from './routes/app.routes';
+
+const routes: Routes = appRoutes;
 
 @NgModule({
   declarations: [AppComponent, HighlightDirective, UnlessDirective],
