259f36944d4fad2ba6526d514b435febbdf0aec5
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-07-06 15:30:52 -0400

    Use interval

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,9 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   title = 'Home';
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // interval(1000).subscribe((count) => {
+    //   // console.log(count);
+    // });
+  }
 }
