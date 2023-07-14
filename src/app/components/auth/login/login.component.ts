04eebb7809c862cdea4bf761b8e4f8cd1d862c4b
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-07-14 13:04:13 -0400

    Add password to user-model

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+    });
   }
-
 }
--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,10 +1,12 @@
 export class User {
   private name: string;
   private email: string;
+  private password: string;
 
-  constructor(name: string, email: string) {
+  constructor(name: string, email: string, password: string = '') {
     this.name = name;
     this.email = email;
+    this.password = password;
   }
 
   getName(): string {
