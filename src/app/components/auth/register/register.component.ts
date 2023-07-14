502234976372f71fa7819aa51f97c4983606d8fb
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-07-14 13:25:41 -0400

    Add register

--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { User } from 'src/app/models/user';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
+  registerForm!: FormGroup;
 
-  constructor() { }
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
+    this.registerForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+    });
   }
 
+  onSubmit() {
+    const registerUser = new User(
+      this.registerForm.value.username,
+      this.registerForm.value.password,
+      this.registerForm.value.email
+    );
+    this.registerForm.reset();
+    this.authService.authenticate(registerUser);
+  }
 }
