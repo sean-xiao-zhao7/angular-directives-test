54be736c9c3848cc4b3d4bfad543b64bba5a9981
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-07-14 12:29:31 -0400

    Add my-interceptor

---
+++ src/app/interceptors/my-interceptor.service.ts
@@ -0,0 +1,22 @@
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MyInterceptorService implements HttpInterceptor {
+  constructor() {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next.handle(req);
+  }
+}
