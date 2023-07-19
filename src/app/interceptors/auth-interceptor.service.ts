ec7c85a03510d566badb770f0d1e5fea2616502d
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-07-19 13:19:44 -0400

    Add base auth-interceptor logic

--- src/app/interceptors/auth-interceptor.service.ts
+++ src/app/interceptors/auth-interceptor.service.ts
@@ -1,9 +1,21 @@
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class AuthInterceptorService {
-
-  constructor() { }
+export class AuthInterceptorService implements HttpInterceptor {
+  constructor() {}
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next.handle(req);
+  }
 }
