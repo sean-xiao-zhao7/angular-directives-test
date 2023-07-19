8e9865bc1da7ce7b563ce98bf611446a1707a7ab
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-07-19 13:24:35 -0400

    Complete auth-interceptor

--- src/app/interceptors/auth-interceptor.service.ts
+++ src/app/interceptors/auth-interceptor.service.ts
@@ -2,20 +2,31 @@ import {
   HttpEvent,
   HttpHandler,
   HttpInterceptor,
+  HttpParams,
   HttpRequest,
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { exhaustMap, take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthInterceptorService implements HttpInterceptor {
-  constructor() {}
+  constructor(private authService: AuthService) {}
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    return next.handle(req);
+    return this.authService.getAuthedUser().pipe(
+      take(1),
+      exhaustMap((user) => {
+        const authReq = req.clone({
+          params: new HttpParams().set('auth', user.idToken),
+        });
+        return next.handle(authReq);
+      })
+    );
   }
 }
