1e5c0650d388695dbbb00f3b53697318cb3fe1d8
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-07-20 15:05:52 -0400

    Allow logout

--- src/app/components/auth/logout/logout.component.ts
+++ src/app/components/auth/logout/logout.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-logout',
   templateUrl: './logout.component.html',
-  styleUrls: ['./logout.component.css']
+  styleUrls: ['./logout.component.css'],
 })
 export class LogoutComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private authService: AuthService) {
+    this.authService.unauthenticate();
   }
 
+  ngOnInit(): void {}
+
+  logout() {
+    this.authService.unauthenticate();
+  }
 }
