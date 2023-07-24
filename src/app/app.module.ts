dca575fa873035e5b62c4cef45cf473d428ffacf
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-07-24 12:47:06 -0400

    Clean up app-module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
-import { HighlightDirective } from './directives/highlight.directive';
-import { UnlessDirective } from './directives/unless.directive';
-import { UsersService } from './services/users.service';
-import { LoggingService } from './services/logging.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
-import { appRoutes } from './routes/app.routes';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
 import { UsersComponent } from './components/users/users.component';
 import { HomeComponent } from './components/home/home.component';
 import { UserComponent } from './components/user/user.component';
@@ -14,14 +12,24 @@ import { UserDetailsComponent } from './components/user-details/user-details.com
 import { EditUserComponent } from './components/edit-user/edit-user.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { ErrorPageComponent } from './components/error-page/error-page.component';
-import { UserResolverService } from './services/user-resolver.service';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HobbyComponent } from './modules/hobby/components/hobby/hobby.component';
+
+import { HighlightDirective } from './directives/highlight.directive';
+import { UnlessDirective } from './directives/unless.directive';
+
+import { UsersService } from './services/users.service';
+import { LoggingService } from './services/logging.service';
+import { UserResolverService } from './services/user-resolver.service';
+
 import { ShortenPipe } from './pipes/shorten.pipe';
 import { FilterPipe } from './pipes/filter.pipe';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+
 import { MyInterceptorService } from './interceptors/my-interceptor.service';
 import { AuthInterceptorService } from './interceptors/auth-interceptor.service';
+
+import { appRoutes } from './routes/app.routes';
+
+// modules
 import { AuthModule } from './modules/auth/auth.module';
 
 const routes: Routes = appRoutes;
