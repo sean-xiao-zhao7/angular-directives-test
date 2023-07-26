b214eacc46db730e6e799606be8d2968e8ad4a0c
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-07-26 14:20:20 -0400

    Remove imports in hobby.module.ts

--- src/app/modules/hobby/hobby.module.ts
+++ src/app/modules/hobby/hobby.module.ts
@@ -1,19 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
 
 import { hobbyRoutes } from './routes/hobby.routes';
-import { UIModule } from '../ui/ui.module';
 import { HobbyComponent } from './components/hobby/hobby.component';
 
 const routes: Routes = hobbyRoutes;
 
 @NgModule({
-  imports: [
-    ReactiveFormsModule,
-    RouterModule.forChild(routes),
-    UIModule,
-    HobbyComponent,
-  ],
+  imports: [RouterModule.forChild(routes), HobbyComponent],
 })
 export class HobbyModule {}
