b360bea32bcf8c3add580cd09f7354df6638e3fd
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-08-02 16:36:45 -0400

    Add hobby test for label name

--- src/app/modules/hobby/components/hobby/hobby.component.spec.ts
+++ src/app/modules/hobby/components/hobby/hobby.component.spec.ts
@@ -1,10 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { StoreModule, Store } from '@ngrx/store';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { HobbyComponent } from './hobby.component';
 import { hobbyReducer } from 'src/app/store/reducers/hobby.reducer';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('HobbyComponent', () => {
   let component: HobbyComponent;
@@ -34,5 +34,10 @@ describe('HobbyComponent', () => {
     let store = fixture.debugElement.injector.get(Store);
     fixture.detectChanges();
     expect(store).toBeTruthy();
+  });
+
+  it('Should render a label with text Name', () => {
+    const template = fixture.debugElement.nativeElement;
+    expect(template.querySelector('label').textContent).toEqual('Name');
   });
 });
