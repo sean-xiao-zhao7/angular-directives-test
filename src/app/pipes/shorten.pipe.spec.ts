2e08830e534c4a181af2ca576308218102bd8d44
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-08-03 12:33:32 -0400

    Add shorten pipe unit test

--- src/app/pipes/shorten.pipe.spec.ts
+++ src/app/pipes/shorten.pipe.spec.ts
@@ -5,4 +5,9 @@ describe('ShortenPipe', () => {
     const pipe = new ShortenPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('shorten the input to 3', () => {
+    const pipe = new ShortenPipe();
+    expect(pipe.transform('test', 3)).toEqual('tes');
+  });
 });
